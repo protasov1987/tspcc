53c6e048b9b80033743136d588f74422f85d3ead
protasov_vds <protasov1987@gmail.com>
2026-01-04 09:50:04 +0000

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -e
 
-cd /var/www/tspcc.ru
+APP_DIR="/var/www/tspcc.ru"
+APP_NAME="tspcc"
+
+cd "$APP_DIR"
 
 echo ">>> Pull from GitHub"
 git pull origin main
 
-echo ">>> Install dependencies"
-npm install --production
+echo ">>> Install dependencies (production only)"
+npm install --omit-dev
 
-echo ">>> Restart pm2"
-pm2 restart tspcc
+echo ">>> Restart pm2 with updated env"
+pm2 restart "$APP_NAME" --update-env
 
 echo ">>> Done!"
