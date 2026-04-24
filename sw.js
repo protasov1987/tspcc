0b371bde94782aa5ab289036afb00aed055679a6
protasov1987 <protasov1987@gmail.com>
2026-04-25 02:11:05 +0300

    Версия 0.16.29: Улучшена видимость выключенной лампочки строк расписания

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '0.16.28';
+const APP_VERSION = '0.16.29';
 const CACHE_VERSION = `pwa-shell-v${APP_VERSION}`;
 const APP_SHELL_CACHE = `tspcc-${CACHE_VERSION}`;
 const APP_SHELL_URLS = [
